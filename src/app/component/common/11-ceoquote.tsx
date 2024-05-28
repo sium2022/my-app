acf08195439e95725e2d309303b0548e17bcdc58
siumhaque1089 <siumhaque1089@gmail.com>
2024-05-28 10:01:57 +0000

    2nd commit from idx

--- src/app/component/common/11-ceoquote.tsx
+++ src/app/component/common/11-ceoquote.tsx
@@ -1,6 +1,14 @@
-function CeoQuote () {
-    return <section>
+function CeoQuote() {
+    return <section className="my-36 mx-60">
+        <p className="text-5xl font-semibold text-center mb-16">The best solution for anyone who wants to work a flexible schedule but still earn a full-time income.</p>
 
+        <div className="text-center">
+
+            <img className="items-center mx-auto rounded-full" src="./ceoquote.jpg" alt="" />
+
+            <p className="mt-6 mb-2 font-semibold text-2xl">Darren Dunlap</p>
+            <p className="text-[#666666] font-medium text-lg">CEO & Founder at Flex.co</p>
+        </div>
     </section>;
 }
 
