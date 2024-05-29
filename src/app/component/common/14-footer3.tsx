10b0541f306fd4e08ffc01949bf5cc0c6cd03e7c
siumhaque1089 <siumhaque1089@gmail.com>
2024-05-29 10:19:28 +0000

    footer3 added

--- src/app/component/common/14-footer3.tsx
+++ src/app/component/common/14-footer3.tsx
@@ -1,6 +1,8 @@
 function Footer3 (){
-    return <section>
-        
+    return <section className="bg-[#F5F7FA]">
+        <hr className="bg-[#FF8B7333] h-1" />
+
+        <p className="text-center py-6 font-normal">@2023 <span className="text-[#FF8166]">CodeStation21</span>. All rights reserved</p>
     </section>;
 }
 
